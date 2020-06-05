392c3d3a4fc78d2b40c9ac93c17fcea9e9fb82ea
Cong Le <longchike@gmail.com>
2020-06-05 14:27:18 -0400

    Initial Commit

---
+++ SocialLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by Cong Le on 6/5/20.
+//  Copyright © 2020 CongLeSolutionX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
